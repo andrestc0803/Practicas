5b80afef2c7fa3df3974b042f15ff2f3b960e5f4
AndresTorres <andrestc010803@gmail.com>
2022-10-03 21:54:18 -0600

    Practica1

---
+++ Practica1LaPerrera/src/test/java/Practica1Perrera/PerreraApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica1Perrera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerreraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
